2058a9ed8b707f79d78b3fc9ad80961fb8fb1073
uday <udaykumar.m@globalsinc.com>
2025-03-16 15:09:56 +0530

    Created Header and Search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
+import Search from './components/Search';
 
 const App = () => {
-  return <div className="text-3xl bg-slate-400 ">Hello World!</div>;
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white ">{searchTerm}</h1>
+      </div>
+    </main>
+  );
 };
 
 export default App;
