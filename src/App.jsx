747511c04425d5bf26ab72c7c7db5e816b51811d
JScoder4005 <muk4005@gmial.com>
2025-05-27 00:40:15 +0530

    Added Search component with search functionality and updated App component to include the Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Search from "./components/Search";
+import { useState } from "react";
 
 const App = () => {
-  return <div className="text-3xl bg-slate-400 ">Hello World!</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="../src/assets/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            without the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  );
 };
 
 export default App;
